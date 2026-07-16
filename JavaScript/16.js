6b264767bf86513f3831649f527e2cdbdea57c48
amaan-shahid <arainamaan08@gmail.com>
2026-07-16 12:21:58 +0500

    MiniGit Project - feat: Add File Logic

--- JavaScript/16.js
+++ JavaScript/16.js
@@ -2,7 +2,18 @@ const createProject = (name) => {
     return {
     name,
     createdAt : Date.now(),   // Current Timestamp in Milli-Seconds
-    files : {}
+    files : {},
+    // ===> We are gonna use objects instead of array of objects because a project with 50000 files would be much more difficult to find in array as in objects we can directly access object properties.
+
+    addFile (filePath, content) {
+        if(this.files[filePath] !== undefined){
+            return "File Already Exists!";
+        }
+        this.files[filePath] = {
+            content,
+            createdAt: Date.now()
+        }
+    }
     }
 };
 
@@ -16,6 +27,13 @@ path.basename(__dirname) → "JavaScript"
 
 */
 
+
+
+// Outouts for testing
+
 const project = createProject(projectName);
+project.addFile("index.html","<h1> Amaan </h1>")
+console.log(project)
+console.log(project.files)
 
 
